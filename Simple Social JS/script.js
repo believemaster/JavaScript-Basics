9e40ebcea4887641918191ee47b0effc0dab00d3
believemaster <believemasters@gmail.com>
2019-11-16 15:26:06 +0530

    Social JS Updates Of validating User

--- Simple Social JS/script.js
+++ Simple Social JS/script.js
@@ -2,6 +2,14 @@ var database = [
   {
     username: "yanik",
     password: "secret"
+  },
+  {
+    username: "nonu",
+    password: "1998"
+  },
+  {
+    username: "bm",
+    password: "2103"
   }
 ];
 
@@ -19,12 +27,31 @@ var newsFeed = [
 var userNamePrompt = prompt("What's your username?");
 var passwordPrompt = prompt("What's your password?");
 
-function signIn(user, pass) {
-  if (user === database[0].username && pass === database[0].password) {
+function isUserValid(username, password) {
+  for (var i = 0; i < database.length; i++) {
+    if (
+      database[i].username === username &&
+      database[i].password === password
+    ) {
+      return true;
+    }
+  }
+  return false;
+}
+
+function signIn(username, password) {
+  if (isUserValid(username, password)) {
     console.log(newsFeed);
   } else {
     alert("Sorry, wrong username and password");
   }
+
+  /*====Simple understanding of array index====*/
+  // if (username === database[0].username && password === database[0].password) {
+  //   console.log(newsFeed);
+  // } else {
+  //   alert("Sorry, wrong username and password");
+  // }
 }
 
 signIn(userNamePrompt, passwordPrompt);
